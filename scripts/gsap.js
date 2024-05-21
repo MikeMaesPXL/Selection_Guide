376310d361c44e6b0b1074837cf0e3e0e63e62a5
MikeMaesPXL <11901518@student.pxl.be>
2024-05-21 16:57:28 +0200

    Added responsiveness for widescreen sizes above 1921px -> Scaled up and reformatted. Removed unneeded text/comments and gave gsap an attempt -> creating email catch before gsap is vital.

---
+++ scripts/gsap.js
@@ -0,0 +1,14 @@
+//Scroll
+// let tl = gsap.timeline({
+//     scrollTrigger: {
+//         trigger: '.animated_title_1',
+//         start: '20% 77.66%',
+//         end: '200% 53.33%',
+//         scrub: true, 
+//         markers: false
+//     }
+// });
+
+// tl.to('.animated_title_1', {
+//     x: 1200
+// })
