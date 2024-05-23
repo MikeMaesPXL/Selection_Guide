4952093321ce0cc37cdddee85d82d6bda555446b
Four <105184275+MikeMWD05@users.noreply.github.com>
2024-05-23 14:37:45 +0200

    Project suddenly changed course and 'irrelevant' pages are being deleted or modified to make the project even more simplistic.

--- scripts/gsap.js
+++ scripts/gsap.js
@@ -1,18 +1,3 @@
-//Scroll
-// let tl = gsap.timeline({
-//     scrollTrigger: {
-//         trigger: '.animated_title_1',
-//         start: '20% 77.66%',
-//         end: '200% 53.33%',
-//         scrub: true, 
-//         markers: false
-//     }
-// });
-
-// tl.to('.animated_title_1', {
-//     x: 1200
-// })
-
 //Buttons color
 const buttons = document.querySelectorAll('.general_style_button');
 
