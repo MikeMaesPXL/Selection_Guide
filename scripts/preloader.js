7ad696a298e514210a2373b5cc21a951938bfa59
Four <105184275+MikeMWD05@users.noreply.github.com>
2024-05-23 09:51:37 +0200

    Final push before the attempt at email + flash/python integration | Changed multiple stylings and removed comments + html edits in each question and answer page. Added responsiveness for mobile landingpage

--- scripts/preloader.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const preloader = document.getElementById('preloader');
-    setTimeout(function() {
-        preloader.style.display = 'none';
-    }, 2000);
-});
