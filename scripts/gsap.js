54f95e91bd6e392d42c4a7dd6f1824cc8d3242ea
Four <105184275+MikeMWD05@users.noreply.github.com>
2024-05-23 12:09:35 +0200

    Added styling to the buttons to create the gsap animation for all buttons

--- scripts/gsap.js
+++ scripts/gsap.js
@@ -12,3 +12,24 @@
 // tl.to('.animated_title_1', {
 //     x: 1200
 // })
+
+//Buttons color
+const buttons = document.querySelectorAll('.general_style_button');
+
+buttons.forEach(button => {
+    const hoverAnimation = gsap.to(button, {
+        background: `linear-gradient(to right, #ffca1d 100%, transparent 100%)`, // Define the hover background gradient
+        backgroundSize: '200% 100%',
+        duration: 0.4,
+        ease: 'power2.inOut', 
+        paused: true,
+    });
+
+    button.addEventListener('mouseenter', () => {
+        hoverAnimation.play(); 
+    });
+
+    button.addEventListener('mouseleave', () => {
+        hoverAnimation.reverse();
+    });
+});
