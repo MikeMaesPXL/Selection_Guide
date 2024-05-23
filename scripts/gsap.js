4049f59f39d71d5105c51be210e3e1dee663404a
MikeMaesPXL <11901518@student.pxl.be>
2024-05-23 18:54:43 +0200

    Last commit before trying out emailJS

--- scripts/gsap.js
+++ scripts/gsap.js
@@ -3,9 +3,9 @@ const buttons = document.querySelectorAll('.general_style_button');
 
 buttons.forEach(button => {
     const hoverAnimation = gsap.to(button, {
-        background: `linear-gradient(to right, #ffca1d 100%, transparent 100%)`,
+        background: `linear-gradient(to top, #ffca1d 100%, transparent 100%)`,
         backgroundSize: '200% 100%',
-        duration: 0.4,
+        duration: 0.5,
         ease: 'power2.inOut', 
         paused: true,
     });
