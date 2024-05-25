1cf73a773b461fb53249dee00cde904fe13fb05e
MikeMaesPXL <11901518@student.pxl.be>
2024-05-25 13:16:49 +0200

    Cursor saved

--- scripts/customCursor.js
+++ scripts/customCursor.js
@@ -1,22 +1,3 @@
-// const storedCursorPosition = JSON.parse(localStorage.getItem('cursorPosition'));
-// let cursorX = storedCursorPosition ? storedCursorPosition.x : 0;
-// let cursorY = storedCursorPosition ? storedCursorPosition.y : 0;
-
-// const cursor = document.querySelector('.custom_cursor');
-// cursor.style.left = cursorX + 'px';
-// cursor.style.top = cursorY + 'px';
-
-// document.addEventListener('mousemove', (e) => {
-//     cursorX = e.pageX - (window.scrollX + 12);
-//     cursorY = e.pageY - (window.scrollY + 14); 
-
-//     cursor.style.left = cursorX + 'px';
-//     cursor.style.top = cursorY + 'px';
-
-//     localStorage.setItem('cursorPosition', JSON.stringify({ x: cursorX, y: cursorY }));
-// });
-
-
 document.addEventListener('DOMContentLoaded', () => {
     const storedCursorPosition = JSON.parse(localStorage.getItem('cursorPosition'));
     let cursorX = storedCursorPosition ? storedCursorPosition.x : 0;
@@ -26,9 +7,8 @@ document.addEventListener('DOMContentLoaded', () => {
     cursor.style.left = cursorX + 'px';
     cursor.style.top = cursorY + 'px';
 
-    let timer; // Initialize the timer variable
+    let timer; 
 
-    // Function to update cursor position
     const updateCursorPosition = (e) => {
         cursorX = e.pageX - (window.scrollX + 12);
         cursorY = e.pageY - (window.scrollY + 14); 
@@ -36,21 +16,19 @@ document.addEventListener('DOMContentLoaded', () => {
         cursor.style.left = cursorX + 'px';
         cursor.style.top = cursorY + 'px';
 
+        cursor.style.transform = 'scale(1.25)';
+
         localStorage.setItem('cursorPosition', JSON.stringify({ x: cursorX, y: cursorY }));
 
-        // Reset the timer whenever the cursor moves
         clearTimeout(timer);
-        cursor.classList.remove('pulse'); // Remove pulse class when cursor moves
+        cursor.classList.remove('pulse');
         timer = setTimeout(() => {
-            // Add pulse class after 3 seconds of inactivity
             cursor.classList.add('pulse');
         }, 3000);
     };
 
-    // Event listener for mousemove
     document.addEventListener('mousemove', updateCursorPosition);
 
-    // Initial setup for cursor color change after 3 seconds of inactivity
     timer = setTimeout(() => {
         cursor.classList.add('pulse');
     }, 3000);
