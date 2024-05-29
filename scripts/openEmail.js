0711556000dbd78fca49bccb04b49a5ddcfcafc8
Four <105184275+MikeMWD05@users.noreply.github.com>
2024-05-29 15:19:05 +0200

    Should be final commit!

--- scripts/openEmail.js
+++ scripts/openEmail.js
@@ -1,4 +1,4 @@
-//Open default email setting upon clicking text
+//Open default email setting by clicking text
 const email = document.getElementById('email_click');
     email.addEventListener('click', function() {
         window.location.href = 'mailto:hello@publilemon.be';
